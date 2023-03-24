b244edae253520d90741321b96563e18a8c947a2
seonzeti <seonzeroticket@gmail.com>
2023-03-24 11:47:04 +0900

    create server

---
+++ day27_React+Node/server.js
@@ -0,0 +1,20 @@
+// 폴더 구성
+// config - 데이터 관리 (DB와 관련된 )
+// public - 정적인 페이지, 사진 등
+// routes - 경로이동
+// views - 동적인 페이지
+
+const express = require("express");
+const app = express();
+const indexRouter = require("./routes");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "react-project", "build")));
+
+app.set("port", process.env.PORT || 8888);
+app.use("/", indexRouter);
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 port에서 대기 중...");
+});
